dad81eaa1b95c07430e671ad93da4d42fc25e890
albobson <alexander.robertson@yale.edu>
2021-12-19 10:42:57 -0500

    Update time cleaning package with the option to change time zones

--- metamessengr/R/clean_mess_time.R
+++ metamessengr/R/clean_mess_time.R
@@ -6,6 +6,8 @@
 #' This will convert the time-stamp to a usable format.
 #'
 #' @param data The original messenger data that was selected using selection()
+#' @param timezone A character vector with the timezone of the user. Default
+#' is Pacific Time.
 #'
 #' @return A data frame with time stamp fixed to be usable in R, and date and
 #' time columns.
@@ -15,12 +17,12 @@
 #' @importFrom magrittr %>%
 #'
 #' @export
-clean_mess_time <- function(data) {
+clean_mess_time <- function(data, timezone="America/Los_Angeles") {
   data %>%
     dplyr::mutate(timestamp_ms = timestamp_ms/1000,
            timestamp_ms = base::as.POSIXct(timestamp_ms,
                                      origin="1970-01-01",
-                                     tz="America/Los_Angeles"),
+                                     tz=timezone),
            date = base::as.Date(timestamp_ms),
            time = base::strftime(timestamp_ms, format="%H:%M:%S"),
            time = chron::chron(times=time),
